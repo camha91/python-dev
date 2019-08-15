aa3ba0154fc7d49c5bf6044cd46012abfe30b761
camha91 <51576355+camha91@users.noreply.github.com>
2019-08-14 22:30:30 -0700

    Update phonebook_func.py

--- Chapter-01-code/phonebook_func.py
+++ Chapter-01-code/phonebook_func.py
@@ -1,28 +1,25 @@
 """
-Testing github
-龙腾测试dev课程，通讯录程序
-实现一个简单的通讯录，包含增删改查
-
-Second clone
+address book program
+Implement a simple address book with additions, deletions, and changes
 """
 record_list = []
 record_id = 0
 
 
 def input_record():
-    name = input("请输入姓名:")
-    phone_number = input("请输入电话:")
+    name = input("Please enter a name:")
+    phone_number = input("Please enter the phone:")
     record = {"name": name, "phone_number": phone_number}
     return record
 
 
 def add_record():
-    record= input_record()
+    record = input_record()
     global record_id
     record_id += 1
     record["record_id"] = record_id
     record_list.append(record)
-    return "添加成功"
+    return "Successfully added"
 
 
 def query_record(name):
@@ -38,22 +35,22 @@ def query_record(name):
 def delete_record(name):
     query_ids, query_result = query_record(name)
     if len(query_ids) == 0:
-        print("不存在")
+        print("Does not exist")
     else:
         if len(query_result) > 1:
             for record in query_result:
                 print("{}\t{}\t{}".format(record["record_id"], record["name"], record["phone_number"]))
-            record_id = input("请选择要删除的id:")
+            record_id = input("Please select the ID you want to delete")
             if int(record_id) in query_ids:
                 for record in record_list:
                     if int(record_id) == record["record_id"]:
                         record_list.remove(record)
             else:
-                print("输入错误!!!")
+                print("Enter error!!!")
         else:
             print("{}\t{}\t{}".format(query_result[0]["record_id"], query_result[0]["name"], query_result[0]["phone_number"]))
             while True:
-                s = input("是否确认删除(Y/N):")
+                s = input("Do you want to confirm deletion (Y/N): ")
                 if s in ["Y", "N"]:
                     if s == "Y":
                         record_list.remove(query_result[0])
@@ -61,69 +58,69 @@ def delete_record(name):
                         pass
                     break
                 else:
-                    print("输入错误!!!")
+                    print("Enter error!!!")
 
 
 def change_record(name):
     query_ids, query_result = query_record(name)
     if len(query_ids) == 0:
-        print("不存在!!!")
+        print("Does not exist!!!")
     else:
         if len(query_result) > 1:
             for record in query_result:
                 print("{}\t{}\t{}".format(record["record_id"], record["name"], record["phone_number"]))
-            record_id = input("请选择要修改的id:")
+            record_id = input("Please select the ID to be modified")
             if int(record_id) in query_ids:
                 for record in record_list:
                     if int(record_id) == record["record_id"]:
-                        phone_number =input("请输入修改后的电话号码:")
+                        phone_number =input("Please enter the modified phone number")
                         record["phone_number"] = phone_number
-                        print("修改成功")
+                        print("Successfully modified")
                         break
             else:
-                print("输入错误!!!")
+                print("Enter error!!!")
         else:
             print("{}\t{}\t{}".format(query_result[0]["record_id"],
                                       query_result[0]["name"], query_result[0]["phone_number"]))
-            phone_number = input("请输入修改后的电话号码:")
+            phone_number = input("Please enter the modified phone number")
             query_result[0]["phone_number"] = phone_number
-            print("修改成功")
+            print("Successfully modified")
 
 
 if __name__ == "__main__":
     while True:
         menu = """
-        通讯录
-        1. 添加
-        2. 查找
-        3. 删除
-        4. 修改
-        5. 退出
+        Address book
+        1. Add
+        2. Find
+        3. Delete
+        4. Modify
+        5. Exit
         """
         print(menu)
-        s = input("请选择操作:")
+        s = input("Please select the operation")
         if s in ["1", "2", "3", "4", "5"]:
 
             if s == "1":
                 msg = add_record()
                 print(msg)
             if s == "2":
-                name = input("请输入姓名:")
+                name = input("Please enter a name:")
                 query_ids, query_result = query_record(name)
                 if len(query_ids) == 0:
-                    print("不存在")
+                    print("Does not exist")
                 else:
                     for record in query_result:
                         print("{}\t{}\t{}".format(record["record_id"], record["name"], record["phone_number"]))
             if s == "3":
-                name = input("请输入姓名:")
+                name = input("Please enter a name:")
                 delete_record(name)
             if s == "4":
-                name = input("请输入姓名:")
+                name = input("Please enter a name:")
                 change_record(name)
             if s == "5":
                 break
         else:
-            print("输入错误")
+            print("Input error")
             continue
 
